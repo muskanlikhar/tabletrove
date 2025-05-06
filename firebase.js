ebd4724c51009fdb35e9ca1edd561fe11d44976c
muskanlikhar <rajasolanki8966@gmail.com>
2025-05-06 13:34:55 +0530

    Update firebase.js

--- firebase.js
+++ firebase.js
@@ -1,8 +1,16 @@
+import { initializeApp } from "https://www.gstatic.com/firebasejs/10.11.0/firebase-app.js";
+import { getFirestore } from "https://www.gstatic.com/firebasejs/10.11.0/firebase-firestore.js";
+
 const firebaseConfig = {
   apiKey: "AIzaSyC_OKwzsTJVBO9ffnsR47sKi3UjA57eu64",
   authDomain: "tabletrove-80cb0.firebaseapp.com",
   projectId: "tabletrove-80cb0",
-  storageBucket: "tabletrove-80cb0.firebasestorage.app",
+  storageBucket: "tabletrove-80cb0.appspot.com",
   messagingSenderId: "124529385870",
   appId: "1:124529385870:web:d821d0a3c6649e7edd04df"
 };
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+const db = getFirestore(app);
+export { db };
